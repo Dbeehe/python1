94afb65e26183c04e0e62a44cfcc53ebff03aa81
Dbeehe <cjh123456a@naver.com>
2023-08-16 11:35:21 +0900

    01_python 1

---
+++ 01_variables/04_tuple.py
@@ -0,0 +1,17 @@
+# 튜플 = 값 변경 불가
+int_tuple = (1, 2, 3, 4)
+print(int_tuple)
+print(int_tuple[1])
+print(int_tuple[2])
+# int_tuple[2] = 10
+print(int_tuple[2])
+
+str_tuple = ("hello", "안녕하세요", "ㅎㅎㅎ")
+print(int_tuple)
+
+
+mix_tuple = (1, 3, "ㅎㅎㅎ", "파이썬")
+
+tuple_in_tuple = ("안녕", (1, 10, "ㅎㅎㅎ"), "hello", 1.23)
+print(tuple_in_tuple)
+
