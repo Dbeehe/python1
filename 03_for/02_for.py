b6ab9a0c4264084510e0f267bb5ab606b42dcb5b
Dbeehe <cjh123456a@naver.com>
2023-08-16 13:49:10 +0900

    python

--- 03_for/02_for.py
+++ 03_for/02_for.py
@@ -6,6 +6,10 @@ list2 = ["가", "나", "다", "라", "마"]
 for i in list2:
     print(i)
 
+# 라스트 2개를 동시에 반복문으로 접근하기
+for i, j in zip(list1, list2):
+    print(i,j)
+
 list3 = ["python", "java", "c", ["javascript", "typescript"]]
 for i in list3:
     print(i)
