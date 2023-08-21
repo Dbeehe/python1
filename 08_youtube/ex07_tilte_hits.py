32e6aaccb438ddfcd97edf5177b4cb178b5e3baa
Dbeehe <cjh123456a@naver.com>
2023-08-21 11:16:20 +0900

    pandas

--- 08_youtube/ex07_tilte_hits.py
+++ 08_youtube/ex07_tilte_hits.py
@@ -5,11 +5,14 @@ import time
 # 크롬 브라우저 실행
 driver = webdriver.Chrome()
 # 접속할 주소
-driver.get("https://www.youtube.com/")
+driver.get("https://www.youtube.com/feed/trending")
 time.sleep(3)
 # 제목 요소 가져오기
 titles = driver.find_elements(By.XPATH, '//*[@id="video-title"]')
-
+# 제목 저장을 위한 리스트
+title_list = []
+# 조회수 저장을 위한 리스트
+hits_list = []
 for title in titles:
     if title.get_attribute("aria-label") and title.text: # shorts 영상을 걸러내기 위한 조건문 
         # aria-label 속성값 가져오기
@@ -20,5 +23,15 @@ for title in titles:
         hits = int(hits.replace(",",""))
         print("제목",title.text)
         print("조회수",hits)
+        # 제목, 조회수를 각각 리스트에 담기
+        # append(): 리스트에 데이터를 추가할 때
+        title_list.append(title.text)
+        hits_list.append(hits)
 
-        
+# 리스트 데이터 확인
+# print("제목리스트", title_list)
+# print("조회수 리스트", hits_list)
+
+# 제목, 조회수를 리스트와 함께 조회
+for title, hit in zip(title_list,hits_list):
+    print(title,hits)      
